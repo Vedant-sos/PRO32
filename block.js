9c39fd7c0a8b619151ef3ff73c119d4d492a8fe9
Vedant-sos <62888002+Vedant-sos@users.noreply.github.com>
2020-09-17 17:05:40 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,35 @@
+class Block{
+    constructor(x,y){
+      var options ={
+          isStatic:false
+      }
+      this.body=Bodies.rectangle(x,y,30,45,options);
+      this.height=45;
+      this.width=30;
+      this.Visibility=255;
+      World.add(world,this.body);
+    }
+
+    score(){
+      if(this.Visibility<0 && this.Visibility>-205){
+      score++;
+      }
+    }
+    display(){
+        if(this.body.speed<3){
+        var pos=this.body.position;
+        fill(rgb(random(10,235),random(20,245),random(30,255)))
+        rectMode(CENTER);
+        rect(pos.x,pos.y,30,45);
+        }
+        else{
+            var pos=this.body.position;
+            World.remove(world,this.body);
+            push();
+            this.Visibility=this.Visibility-5;
+            tint(255,this.Visibility);
+            //rect(pos.x,pos.y,30,45);
+            pop();
+        }
+    }
+}
